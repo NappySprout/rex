1799189d969e560cf167afd7916bc66facbeaf69
NappySprout <sleepylog20@gmail.com>
2023-03-14 00:51:03 +0800

    allowed cors

--- src/server/app.js
+++ src/server/app.js
@@ -2,8 +2,10 @@ import express from "express"
 import auth from "./routes/auth/index.js"
 import api from "./routes/api/index.js"
 import view from "./routes/view/index.js"
+import cors from "cors"
 const app = express()
 //middleware
+app.use(cors())
 app.use(express.json())
 app.use('/auth', auth)
 app.use('/api', api)
